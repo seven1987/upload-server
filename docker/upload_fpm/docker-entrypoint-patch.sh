afad80e59755f5164b46f3801639317d5a171d51
zhangwen <zhangwen@sensefun.com>
2018-01-04 15:36:33 +0800

    docker 启动异常

--- docker/upload_fpm/docker-entrypoint-patch.sh
+++ docker/upload_fpm/docker-entrypoint-patch.sh
@@ -4,7 +4,4 @@
 
 (cd /var/www/html/src; [ `ls public/uploads|wc -w` -ne 0 ]||cp -r uploads/* public/uploads)
 
-echo "############################################################"
-echo "execute ${BASH_SOURCE}"
-echo 
-source docker-entrypoint.sh
+exec "$@"
