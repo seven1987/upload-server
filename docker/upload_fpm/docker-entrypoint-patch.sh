2890c90f16482771515563bc86169ee799afc682
zhangwen <zhangwen@sensefun.com>
2018-01-04 15:42:54 +0800

    docker-entrypoint-patch.sh 恢复

--- docker/upload_fpm/docker-entrypoint-patch.sh
+++ docker/upload_fpm/docker-entrypoint-patch.sh
@@ -3,3 +3,8 @@
 #set -x
 
 (cd /var/www/html/src; [ `ls public/uploads|wc -w` -ne 0 ]||cp -r uploads/* public/uploads)
+
+echo "############################################################"
+echo "execute ${BASH_SOURCE}"
+echo
+source docker-entrypoint.sh
